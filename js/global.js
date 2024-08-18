059013cfba0001e3d3c33726f90a85dd1f39df46
vickymiau35 <168245335+vickymiau35@users.noreply.github.com>
2024-08-18 20:59:28 -0300

    Delete js/global.js

--- js/global.js
+++
@@ -1,8 +0,0 @@
-
-let traerUsuario=JSON.parse(localStorage.getItem('datosUsuario')) || null;
-
-
-    if(traerUsuario?.rol!=='admin')
-        {document.getElementById("administrar").innerHTML = ""}
-
-validarUsuario();
