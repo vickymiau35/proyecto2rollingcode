8412797e6bf779f1c0d5df17a1b01e6d8ec6ed62
Juan Loretto <juanloretto29@gmail.com>
2024-08-18 18:16:28 -0300

    Agrego slide de cards para detalles

--- js/detalles.js
+++ js/detalles.js
@@ -1,124 +1,176 @@
-const contenedorComentarios = document.getElementById('contenedorComentarios');
+
+const contenedorComentarios = document.getElementById("contenedorComentarios");
 function crearComentarioEstructura(nombre, comentario) {
-    // Crea la estructura del comentario
-    const comentarioDiv = document.createElement('div');
-    comentarioDiv.classList.add('media', 'mt-3');
+  // Crea la estructura del comentario
+  const comentarioDiv = document.createElement("div");
+  comentarioDiv.classList.add("media", "mt-3");
 
-    const mediaBodyDiv = document.createElement('div');
-    mediaBodyDiv.classList.add('media-body');
+  const mediaBodyDiv = document.createElement("div");
+  mediaBodyDiv.classList.add("media-body");
 
-    const nombreH5 = document.createElement('h5');
-    nombreH5.classList.add('mt-0');
-    nombreH5.textContent = nombre;
+  const nombreH5 = document.createElement("h5");
+  nombreH5.classList.add("mt-0");
+  nombreH5.textContent = nombre;
 
-    const comentarioP = document.createElement('p');
-    comentarioP.textContent = comentario;
+  const comentarioP = document.createElement("p");
+  comentarioP.textContent = comentario;
 
-    mediaBodyDiv.append(nombreH5, comentarioP);
-    comentarioDiv.append(mediaBodyDiv);
+  mediaBodyDiv.append(nombreH5, comentarioP);
+  comentarioDiv.append(mediaBodyDiv);
 
-    return comentarioDiv;
+  return comentarioDiv;
 }
 
 function agregarComentario() {
-    const nombre = document.getElementById('nombreUsuario').value;
-    const comentario = document.getElementById('comentarioUsuario').value;    
-    const comentarios = JSON.parse(localStorage.getItem('comentarios')) || [];
+  const nombre = document.getElementById("nombreUsuario").value;
+  const comentario = document.getElementById("comentarioUsuario").value;
 
-    // Crea un nuevo comentario
-    const nuevoComentario = {
-        nombre: nombre,
-        comentario: comentario
-    };
-    comentarios.push(nuevoComentario);
+  const comentarios = JSON.parse(localStorage.getItem("comentarios")) || [];
 
-    localStorage.setItem('comentarios', JSON.stringify(comentarios));
+  // Crea un nuevo comentario
+  const nuevoComentario = {
+    nombre: nombre,
+    comentario: comentario,
+  };
+  comentarios.push(nuevoComentario);
 
-    //Creamos y agregamos al dom la estructura del comentario
-    const comentarioDiv = crearComentarioEstructura(nombre, comentario);
-    contenedorComentarios.appendChild(comentarioDiv);
-    
+  localStorage.setItem("comentarios", JSON.stringify(comentarios));
+
+  //Creamos y agregamos al dom la estructura del comentario
+  const comentarioDiv = crearComentarioEstructura(nombre, comentario);
+  contenedorComentarios.appendChild(comentarioDiv);
 }
 
 // Función para cargar los comentarios desde el localStorage cuando cargue el DOM
 function cargarComentarios() {
-    const comentarios = JSON.parse(localStorage.getItem('comentarios')) || [];
+  const comentarios = JSON.parse(localStorage.getItem("comentarios")) || [];
 
-    comentarios.forEach(comentario => {
-        const comentarioDiv = crearComentarioEstructura(comentario.nombre, comentario.comentario);
-        contenedorComentarios.appendChild(comentarioDiv);
-    });
+  comentarios.forEach((comentario) => {
+    const comentarioDiv = crearComentarioEstructura(
+      comentario.nombre,
+      comentario.comentario
+    );
+    contenedorComentarios.appendChild(comentarioDiv);
+  });
 }
 
+document.addEventListener("DOMContentLoaded", cargarComentarios);
 
-document.addEventListener('DOMContentLoaded', cargarComentarios);
+document.addEventListener("DOMContentLoaded", () => {
+  const url = new URL(location.href);
+  const idviaje = url.searchParams.get("id");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const url = new URL(location.href);  
-    const idviaje = url.searchParams.get('id'); 
+  const arrayProductos = JSON.parse(localStorage.getItem("productos")) || [];
+  const viaje = arrayProductos.find((item) => item.id == idviaje);
 
-    
-    const arrayProductos = JSON.parse(localStorage.getItem("productos")) || [];
-    const viaje = arrayProductos.find(item => item.id == idviaje); 
-
-    if (viaje) {
-        updateProduct(viaje);
-    } else {
-        console.error('Producto no encontrado');
-    }
+  if (viaje) {
+    updateProduct(viaje);
+  } else {
+    console.error("Producto no encontrado");
+  }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const url = new URL(location.href);  
-    const idviaje = url.searchParams.get('id'); 
+    const categoria = viaje.categoria;
+    console.log(categoria); 
+    
+  const productoRelacionado = arrayProductos.filter(
+    (producto) => producto.categoria === categoria && producto.id !== idviaje
+  );
+ console.log(productoRelacionado)
+  
+ const contenedor = document.getElementById("div-relacionados");
+  productoRelacionado.forEach((producto) => {
+    const card = document.createElement("div");
+    card.classList.add("card"); // Clase necesaria para Swiper
 
-   
-    const arrayProductos = JSON.parse(localStorage.getItem("productos")) || [];
-    const viaje = arrayProductos.find(item => item.id == idviaje); 
-
-    if (viaje) {
-        updateProduct(viaje);
-    } else {
-        console.error('Producto no encontrado');
+    card.innerHTML = `
+      
+        <div class="card-img-container">
+          <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+          <div class="badge-container">
+            <i class="bi bi-luggage-fill"></i>
+            <span>Paquete</span>
+          </div>
+        </div>
+        <div class="card-body">
+          <h4 class="card-title fs-5">${producto.nombre}</h4>
+          <p class="card-text fs-6 fw-medium">${producto.duracion}</p>
+          <p class="card-text fs-6 my-0">Alojamiento: ${producto.alojamiento}</p>
+          <hr class="card-separator">
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="info-container">
+              <p class="card-text fs-5 mb-1 precio">${producto.precio}</p>
+              <a href="#" onclick="irViaje(${producto.id})" class="text-decoration-none fs-6">Ver más...</a>
+            </div>                       
+          </div>
+        </div>
+      
+    `;
+    contenedor.append(card);
+  
+  function scrollCarousel(sectionId,direction) {
+    const container = document.getElementById(sectionId)
+    if (!container) return;  // Verifica si el contenedor existe
+    const cardWidth = container.querySelector('.card').offsetWidth;
+    const scrollAmount = cardWidth + 16
+  
+    if (direction === 1) {
+        container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    } else if (direction === -1) {
+        container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
     }
+  }
+
+
+
+  if (viaje) {
+    updateProduct(viaje);
+  } else {
+    console.error("Producto no encontrado");
+  }
 });
 
 function updateProduct(viaje) {
-    document.getElementById('nombre').innerText = viaje.nombre;
-    document.getElementById('categoria').innerText = viaje.categoria;
-    document.getElementById('precio').innerText = viaje.precio;
-    document.getElementById('descripcion').innerText = viaje.descripcion;
+  document.getElementById("nombre").innerText = viaje.nombre;
+  document.getElementById("categoria").innerText = viaje.categoria;
+  document.getElementById("precio").innerText = viaje.precio;
+  document.getElementById("descripcion").innerText = viaje.descripcion;
 
-    let lista = `<ul>
+  let lista = `<ul>
         <li>Fecha de salida: ${viaje.fecha_de_salida}</li>
         <li>Fecha de regreso: ${viaje.fecha_de_regreso}</li>
         <li>Alojamiento: ${viaje.alojamiento}</li>
     </ul>`;
 
-    const CONT_LIST = document.getElementById('contlist');
-    CONT_LIST.innerHTML = lista
-    // CONT_LIST.append(lista)
+  const CONT_LIST = document.getElementById("contlist");
+  CONT_LIST.innerHTML = lista;
+  // CONT_LIST.append(lista)
 
-    //Cambiar imagen
-    const DINAMIC_IMG = document.querySelector('#productImageDisplay')
-    DINAMIC_IMG.src = viaje.imagen;
-
+  //Cambiar imagen
+  const DINAMIC_IMG = document.querySelector("#productImageDisplay");
+  DINAMIC_IMG.src = viaje.imagen;
 }
-function clearBox(){
-    document.getElementById('nombreUsuario').value = '' 
-    document.getElementById('comentarioUsuario').value = ''
-}
-const AGREGAR_COMENT = document.getElementById('coment-btn')
-AGREGAR_COMENT.addEventListener('click', clearBox())
+document.addEventListener("DOMContentLoaded", () => {
+  const btnContactoAsesor = document.getElementById("contacto-asesor");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const btnContactoAsesor = document.getElementById('contacto-asesor');
+  btnContactoAsesor.addEventListener("click", () => {
+    const numeroTelefono = "543816817725";
+    const mensaje = encodeURIComponent(
+      "Hola, estoy interesado en obtener más información sobre el viaje"
+    );
+    const urlWhatsApp = `https://wa.me/${numeroTelefono}?text=${mensaje}`;
 
-    btnContactoAsesor.addEventListener('click', () => {
-        const numeroTelefono = '543816817725'; 
-        const mensaje = encodeURIComponent('Hola, estoy interesado en obtener más información sobre el viaje');
-        const urlWhatsApp = `https://wa.me/${numeroTelefono}?text=${mensaje}`;
-        
-        window.open(urlWhatsApp, '_blank'); 
-    });
+    window.open(urlWhatsApp, "_blank");
+  });
 });
+function scrollCarousel(containerId, direction) {
+    const container = document.getElementById(containerId);
+    const slideWidth = container.querySelector('.swiper-slide').offsetWidth;
+    container.scrollBy({
+      left: direction * slideWidth, // Desplazar en la dirección adecuada
+      behavior: 'smooth', // Desplazamiento suave
+    });
+  }
+  function irViaje(id){
+    location.href=`../pages/detalles.html?id=${id}`
+}
